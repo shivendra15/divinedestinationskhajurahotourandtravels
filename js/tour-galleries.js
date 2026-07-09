53f9397244931984bedc15fc4f0075b03446dffe
Shivendra Singh Parihar <shivendra150192@gmail.com>
2026-07-09 07:54:38 -0700

    Create TOUR_GALLERIES object with travel plans
    Added a TOUR_GALLERIES object with three plans, each containing titles, English titles, and associated photo paths.

---
+++ js/tour-galleries.js
@@ -0,0 +1,31 @@
+const TOUR_GALLERIES = {
+    planA: {
+        title: "世界遺産カジュラホ 終日観光",
+        titleEn: "Khajuraho World Heritage Full Day",
+        photos: [
+            "images/destinations/khajuraho/the_kandariya_khajuraho.jpg",
+            "images/destinations/khajuraho/kandariya_mahadev_temple_khajuraho.jpg",
+            "images/destinations/khajuraho/devi_jagdambi_temple_khajuraho.jpg",
+            "images/destinations/khajuraho/kandariya_temple_khajuraho.jpg",
+            "images/destinations/khajuraho/lion_sculptures_khajuraho.jpg",
+            "images/destinations/khajuraho/nagra_style_erotic_carving_kandariya_khajuraho.jpg"
+        ]
+    },
+    planB: {
+        title: "カジュラホ＆パナ国立公園サファリ",
+        titleEn: "Khajuraho & Panna National Park Safari",
+        photos: [
+            "images/destinations/khajuraho/kandariya_temple_khajuraho.jpg",
+            "images/destinations/khajuraho/the_kandariya_khajuraho.jpg"
+        ]
+    },
+    planC: {
+        title: "北インド周遊アレンジ",
+        titleEn: "North India Custom Arrangements",
+        photos: [
+            "images/destinations/golden-triangle/t1_IronPillarNight_home.jpg",
+            "images/destinations/golden-triangle/t1_IndiaGateNight.jpg",
+            "images/destinations/delhi/qutub_minar_night_3.jpg"
+        ]
+    }
+};
