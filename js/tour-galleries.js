c496919eb4a23a8ea425d13a9ab324c13ea5e7ef
Shivendra Singh Parihar <shivendra150192@gmail.com>
2026-07-20 01:00:57 -0700

    Update tour gallery plans with new titles and photos

--- js/tour-galleries.js
+++ js/tour-galleries.js
@@ -1,7 +1,32 @@
-const TOUR_GALLERIES = {
+const TOUR_GALLERIES = {    
     planA: {
-        title: "世界遺産カジュラホ 終日観光",
-        titleEn: "Khajuraho World Heritage Full Day",
+        title: "インペリアル・クラシック",
+        titleEn: "Imperial Classic",
+        photos: [
+            "images/destinations/golden-triangle/t1_IronPillarNight_home.jpg",
+            "images/destinations/golden-triangle/t1_IndiaGateNight.jpg",
+            "images/destinations/delhi/qutub_minar_night_3.jpg"
+        ]
+    },
+    planB: {
+        title: "精神の道",
+        titleEn: "The Spiritual Path",
+        photos: [
+            "images/destinations/khajuraho/kandariya_temple_khajuraho.jpg",
+            "images/destinations/khajuraho/the_kandariya_khajuraho.jpg"
+        ]
+    },
+    planC: {
+        title: "古代の響き",
+        titleEn: "Ancient Echoes",
+        photos: [
+            "images/destinations/khajuraho/kandariya_temple_khajuraho.jpg",
+            "images/destinations/khajuraho/the_kandariya_khajuraho.jpg"
+        ]
+    },
+    planD: {
+        title: "世界遺産カジュラホ",
+        titleEn: "Khajuraho World Heritage Site",
         photos: [
             "images/destinations/khajuraho/the_kandariya_khajuraho.jpg",
             "images/destinations/khajuraho/kandariya_mahadev_temple_khajuraho.jpg",
@@ -9,23 +34,6 @@ const TOUR_GALLERIES = {
             "images/destinations/khajuraho/kandariya_temple_khajuraho.jpg",
             "images/destinations/khajuraho/lion_sculptures_khajuraho.jpg",
             "images/destinations/khajuraho/nagra_style_erotic_carving_kandariya_khajuraho.jpg"
-        ]
-    },
-    planB: {
-        title: "カジュラホ＆パナ国立公園サファリ",
-        titleEn: "Khajuraho & Panna National Park Safari",
-        photos: [
-            "images/destinations/khajuraho/kandariya_temple_khajuraho.jpg",
-            "images/destinations/khajuraho/the_kandariya_khajuraho.jpg"
-        ]
-    },
-    planC: {
-        title: "北インド周遊アレンジ",
-        titleEn: "North India Custom Arrangements",
-        photos: [
-            "images/destinations/golden-triangle/t1_IronPillarNight_home.jpg",
-            "images/destinations/golden-triangle/t1_IndiaGateNight.jpg",
-            "images/destinations/delhi/qutub_minar_night_3.jpg"
         ]
     }
 };
